46c1efa968216421058d36a7bd6079dc15a00bbd
Asm Shad (INT) <asm.shad@liz.com>
2024-09-24 23:19:07 +0600

    Feni and Quota Functionality Complete

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 
-//const noakhaliInput = getInputFieldValueById('noakhaliInputField');
-// const feniInput = getInputFieldValueById('feniInputField');
-// const quotaInput = getInputFieldValueById('quotaInputField');
-
-// const userBalance = getTextFieldValueById('userBalanceAmount');
-// const noakhaliBalance = getTextFieldValueById('noakhaliBalanceAmount');
-// const feniBalance = getTextFieldValueById('feniBalanceAmount');
-// const quotaBalance = getTextFieldValueById('quotaBalanceAmount');
-
-//console.log(noakhaliInput);
-
 const historyField = document.getElementById('history-view');
 const donationField = document.getElementById('donaton-view');
 
@@ -49,6 +38,60 @@ document.getElementById('donate-noakhali').addEventListener('click', function(){
 
         historyItem.innerHTML = `
             <h3 class="font-bold">${noakhaliInput} Taka is Donated for Flood Relief in Noakhali, Bangladesh</h3>
+            <p class="text-xs text-gray-500">Date: ${new Date().toString()}</p>
+        `;
+        const historyContainer = document.getElementById('historyField');
+        historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+    }
+});
+
+document.getElementById('donate-feni').addEventListener('click', function(){
+    const feniInput = getInputFieldValueById('feniInputField');
+    const userBalance = getTextFieldValueById('userBalanceAmount');
+    const feniBalance = getTextFieldValueById('feniBalanceAmount');
+
+    if(feniInput > userBalance){
+        alert('Invalid Data');
+    } 
+    else {
+        const userNewBalance = userBalance - feniInput;
+        const feniNewBalance = feniBalance + feniInput;
+
+        document.getElementById('userBalanceAmount').innerText = userNewBalance;
+        document.getElementById('feniBalanceAmount').innerText = feniNewBalance;
+
+        const historyItem = document.createElement('div');
+        historyItem.className = 'bg-white p-4'
+
+        historyItem.innerHTML = `
+            <h3 class="font-bold">${feniInput} Taka is Donated for Flood Relief in Feni, Bangladesh</h3>
+            <p class="text-xs text-gray-500">Date: ${new Date().toString()}</p>
+        `;
+        const historyContainer = document.getElementById('historyField');
+        historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+    }
+});
+
+document.getElementById('donate-quota').addEventListener('click', function(){
+    const quotaInput = getInputFieldValueById('quotaInputField');
+    const userBalance = getTextFieldValueById('userBalanceAmount');
+    const quotaBalance = getTextFieldValueById('quotaBalanceAmount');
+
+    if(quotaInput > userBalance){
+        alert('Invalid Data');
+    } 
+    else {
+        const userNewBalance = userBalance - quotaInput;
+        const quotaNewBalance = quotaBalance + quotaInput;
+
+        document.getElementById('userBalanceAmount').innerText = userNewBalance;
+        document.getElementById('quotaBalanceAmount').innerText = quotaNewBalance;
+
+        const historyItem = document.createElement('div');
+        historyItem.className = 'bg-white p-4'
+
+        historyItem.innerHTML = `
+            <h3 class="font-bold">${quotaInput} Taka is Donated for Aid to injured in Quota movement</h3>
             <p class="text-xs text-gray-500">Date: ${new Date().toString()}</p>
         `;
         const historyContainer = document.getElementById('historyField');
